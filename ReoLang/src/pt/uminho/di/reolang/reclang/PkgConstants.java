682b12e7b74542e134f0c42400257aedae76cc1d
FlavioR <pg22826@alunos.uminho.pt>
2014-04-24 23:42:55 +0100

    Minor Changes on ReCooPLa input examples; Added PkgConstants class

---
+++ ReoLang/src/pt/uminho/di/reolang/reclang/PkgConstants.java
@@ -0,0 +1,11 @@
+package pt.uminho.di.reolang.reclang;
+
+public final class PkgConstants {
+	
+	public static final String JAVASSIST = "javassist";
+	public static final String JAVA_UTIL = "java.util";
+	public static final String CP_MODEL = "pt.uminho.di.cp.model";
+	public static final String CP_RECONFIGURATIONS = "pt.uminho.di.cp.reconfigurations";
+	public static final String REOLANG_PARSING_UTIL = "pt.uminho.di.reolang.parsing.util";
+	
+}
